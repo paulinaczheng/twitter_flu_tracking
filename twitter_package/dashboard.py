46b4a2d067cd007bc0bad2b2eb36373cbeb1ac6b
Paulina Zheng <paulinaczheng@gmail.com>
2018-10-03 09:44:59 -0400

    add individual roc curves to dash

--- twitter_package/dashboard.py
+++ twitter_package/dashboard.py
@@ -99,13 +99,16 @@ app.layout = html.Div(style={'fontFamily': 'Sans-Serif'}, children=[
                 {'label': 'Support Vector Machine', 'value': 'svm'}
                         ],
                 placeholder="Select a Model", value ='Model'),
+                dcc.Markdown('***'),
                 html.Div(id='cm-container'),
-                html.Div([dcc.Graph(id='roc',figure=generate_all_roc_curves())]),
+                # html.Div([dcc.Graph(id='roc',figure=generate_all_roc_curves())]),
+                html.Div(id='roc-container')
                         ]),
         dcc.Tab(label='Time Series Analysis', children=[
             html.Div([
                 html.H1('Time Series Analysis Overview'),
                 html.Div([html.Img(src='data:image/png;base64,{}'.format(encoded_sarima_image.decode()))]),
+                dcc.Markdown('***'),
                 dcc.Dropdown(
                 id='select-arima-metrics',
                 options=[{'label': 'Visualizations', 'value': 'visual'},
@@ -115,6 +118,7 @@ app.layout = html.Div(style={'fontFamily': 'Sans-Serif'}, children=[
                 {'label': 'Forecasting', 'value': 'forecasting'},
                         ],
                 placeholder="Select Model Metrics", value ='Metric'),
+                dcc.Markdown('***'),
                 html.Div(id='ts-container'),
                         ])
                         ]),
@@ -122,13 +126,14 @@ app.layout = html.Div(style={'fontFamily': 'Sans-Serif'}, children=[
             html.Div([
                 html.H1('Conclusions'),
                 dcc.Markdown('* Logistic regression was the best-performing classifier, with TF-IDF vectorization (with trigrams) used to process the annotated tweets'),
-                dcc.Markdown('* The SARIMA model that included both CDC & Twitter data did better at one-step ahead forecasting than the SARIMA model with just CDC data, using RMSE as a metric.'),
+                dcc.Markdown('* The SARIMA model that included both CDC & Twitter data did better at one-step ahead forecasting than the SARIMA model with just CDC data, using RMSE as a metric (3651.55 vs. 3448.62).'),
                 dcc.Markdown('* This implies that flu-related tweets contribute to the SARIMA model in some way that improves the predictive ability of the SARIMA model'),
                 html.H1('Limitations'),
                 dcc.Markdown('* ARIMA models very dependent on data trends and characteristics, requiring frequent refitting of model parameters'),
                 dcc.Markdown('* Poor ability to prospectively detect outbreaks'),
                 html.H1('Next Steps'),
                 dcc.Markdown('* Add more annotated tweets to train classifiers on'),
+                dcc.Markdown('* Incorporate deep learning (recurrent neural networks) to classify tweets'),
                 dcc.Markdown('* Include Google Trends data in time-series analyses'),
                 dcc.Markdown('* Use other time-series libraries/models for better forecasting (VARIMA, FBProphet)'),
                         ])
@@ -161,6 +166,39 @@ def generate_confusion_matrix(input_value):
     trace = [go.Heatmap(x=['POS', 'NEG'], y=['POS', 'NEG'], z=cm)]
     return dcc.Graph(id ='heatmap', figure = go.Figure(data = trace))
 
+@app.callback(Output(component_id = 'roc-container', component_property ='children'),
+[Input(component_id = 'select-model',component_property = 'value')])
+def generate_roc_curve(input_value):
+    data = []
+    lw=2
+    model = check_model(input_value)
+    model.fit(x_train, y_train)
+    model_name = generate_classifier_name(model)
+    if model==log:
+        y_score = model.decision_function(x_test)
+        fpr, tpr, thresholds = roc_curve(y_test, y_score)
+    else:
+        y_score = model.predict_proba(x_test)
+        fpr, tpr, thresholds = roc_curve(y_test, y_score[:,1])
+    roc_auc = auc(fpr, tpr)
+    trace = go.Scatter(x=fpr, y=tpr,
+                       mode='lines',
+                           line=dict(width=lw, color='b'),
+                       name='{} (area = {})'.format(model_name, round(roc_auc,2)))
+    data.append(trace)
+    trace = go.Scatter(x=[0, 1], y=[0, 1],
+               mode='lines',
+               line=dict(width=lw, color='black', dash='dash'),
+               name='Luck')
+    data.append(trace)
+    layout = go.Layout(title='Receiver Operating Characteristic (ROC) Curve',
+                       xaxis=dict(title='False Positive Rate', showgrid=False,
+                                  range=[-0.05, 1.05]),
+                       yaxis=dict(title='True Positive Rate', showgrid=False,
+                                  range=[-0.05, 1.05]))
+    fig = go.Figure(data=data, layout=layout)
+    return dcc.Graph(id='roc', figure={'data': fig,
+                    'layout': layout})
 # @app.callback(Output(component_id = 'vec-container', component_property ='children'),
 # [Input(component_id = 'select-vectorizer-metrics',component_property = 'value')])
 # def generate_vectorization_metrics(input_value):
@@ -183,7 +221,7 @@ def generate_vectorization_metrics(input_value):
     elif input_value=='diagnostics':
         return diagnostics_plots()
     elif input_value=='forecasting':
-        return 'Forecasting'
+        return forecasting_plots()
 
 @app.callback(Output('table', 'rows'), [Input('field-dropdown', 'value')])
 def update_table(user_selection):
